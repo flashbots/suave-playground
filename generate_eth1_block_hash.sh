4951189dd574f1f429d0fdaf7bfa8162c026d258
Ferran Borreguero <ferran.borreguero@gmail.com>
2024-07-06 17:57:43 +0100

    Interim solution

---
+++ generate_eth1_block_hash.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Use reth to generate the block hash
+# reth node --datadir /tmp --chain ./genesis.json --http
+# cast block 0 --json | jq ".hash" --raw-output
+
+# ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+# 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
